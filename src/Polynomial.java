84ceeaff9efb2f46d3e321ce5622b448147bbff5
DarthMooMancer <107283522+DarthMooMancer@users.noreply.github.com>
2025-05-06 08:33:42 -0400

    Feat: Added toString and equals methods to Polynomial

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -17,4 +17,15 @@ class Polynomial {
     public int get_terms() {
 	return this.terms;
     }
+
+    public String toString() {
+	return String.format("Terms: %s, Degrees: %s", get_terms(), get_degree());
+    }
+
+    public boolean equals(Object obj) {
+	if(this == obj) return true;
+	if (obj == null || getClass() != obj.getClass()) return false;
+        Polynomial other = (Polynomial) obj;
+        return degree == other.degree && terms == other.terms;
+    }
 }
