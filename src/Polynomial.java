31136e2c3ffba5fcf3979d2e0f496e930f5df12e
DarthMooMancer <107283522+DarthMooMancer@users.noreply.github.com>
2025-05-06 08:28:43 -0400

    adding main functions to polynomial

--- src/Polynomial.java
+++ src/Polynomial.java
@@ -1,3 +1,20 @@
-public class Polynomial {
-    // New File
+class Polynomial {
+    private int degree;
+    private int terms;
+
+    public Polynomial(int degree) {
+	if(degree < 0) {
+	    this.degree = 0;
+	} else {
+	    this.degree = degree;
+	}
+    }
+
+    public int get_degree() {
+	return this.degree;
+    }
+
+    public int get_terms() {
+	return this.terms;
+    }
 }
